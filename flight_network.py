cc5e649d7210fd3bb34d6b4e2f59cc22b634976a
beatricemazz <115489956+beatricemazz@users.noreply.github.com>
2024-12-16 17:40:17 +0100

    Add files via upload

--- flight_network.py
+++ flight_network.py
@@ -1,26 +1,33 @@
-#flight_network.py
-from typing import Dict, List, Set, Tuple
-from collections import defaultdict
+import numpy as np
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import io
+import base64
+import networkx as nx
+from typing import Dict, List, Tuple
 
 class FlightNetwork:
+
+    
     def __init__(self):
-        self.nodes: Set[str] = set()
-        self.edges: Set[Tuple[str, str]] = set()
-        self.in_edges: Dict[str, List[str]] = defaultdict(list)
-        self.out_edges: Dict[str, List[str]] = defaultdict(list)
+        
+        self.graph = nx.DiGraph()
 
     def add_nodes_and_edges(self, origin_airports: pd.Series, destination_airports: pd.Series) -> None:
-        self.nodes.update(origin_airports)
-        self.nodes.update(destination_airports)
-        self.edges.update(zip(origin_airports, destination_airports))
         
-        for origin, destination in zip(origin_airports, destination_airports):
-            self.in_edges[destination].append(origin)
-            self.out_edges[origin].append(destination)
+        self.graph.add_nodes_from(origin_airports.unique())
+        self.graph.add_nodes_from(destination_airports.unique())
+        edges = list(zip(origin_airports, destination_airports))
+        self.graph.add_edges_from(edges)
 
-    def in_degree(self, node: str) -> int:
-        return len(self.in_edges[node])
+    @property
+    def nodes(self) -> List[str]:
+        
+        return list(self.graph.nodes())
 
-    def out_degree(self, node: str) -> int:
-        return len(self.out_edges[node])
+    @property
+    def edges(self) -> List[tuple]:
+       
+        return list(self.graph.edges())
+
